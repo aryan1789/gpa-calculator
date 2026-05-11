cf0f4aa6afff874021db3bcf74231acd91886b4b
Aryan Shah <qgm3818@aut.ac.nz>
2026-05-12 09:35:48 +1200

    Basic initial commit (created dec 2024)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,222 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function calculateGPA(grades) {
+//   const gpa = totalGPASum / noOfCourses;
+//   const honGPA = (L6GPA + L7GPA*2+L8GPA*3)/((noOfL6*9)+(noOfL7*9*2)+(noOfL8*9*3))*9;
+//   return { gpa, honGPA };
+// }
+
+function display() {
+  
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="App-logo" />
+        <h1> Your GPA is : 4.0
+        </h1>
+        <h1>
+          Your Honours GPA is : 4.0
+        </h1>
+        
+        <form>
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <label for="fname">Class:
+  <input type="text" id="fname" name="fname"/></label>
+  <label for="grade">Grade:
+  <select type="text" id="grade" name="grade">
+    <option value="A+">A+</option>
+    <option value="A">A</option>
+    <option value="A-">A-</option>
+    <option value="B+">B+</option>
+    <option value="B">B</option>
+    <option value="B-">B-</option>
+    <option value="C+">C+</option>
+    <option value="C">C</option>
+    <option value="C-">C-</option>
+  </select></label>
+  <label for="level">Level:
+  <select id="level" name="level">
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    </select>
+    </label>
+  <br></br>
+
+  <input type="submit" value="Calculate GPA"/>
+</form>
       </header>
     </div>
   );
